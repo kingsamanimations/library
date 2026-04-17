696a789293f87d4b6a4fb8bb5b8374656754a581
King Sam Animations <224757196+kingsamanimations@users.noreply.github.com>
2026-04-17 11:30:38 +0100

    Refactored books.js to use class instead of constructors

--- books.js
+++ books.js
@@ -1,115 +1,127 @@
+//Refactored into Class
+
 // Create book and add to library
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  // the constructor...
-  this.title = title; // The this value is included here
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    // the constructor...
+    this.title = title; // The this value is included here
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 // Render the output into myLibrary
-function render() {
-  const display = document.getElementById('library');
-  display.innerHTML = ""; // To prevent duplicate copies
-  const books = document.querySelectorAll('book');
-  books.forEach(book => display.removeChild(book));
-  
-  let libraryBook = document.querySelector("#library");
-  // Go over the myLibrary array and display each book that's added
-  
-  for (let i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary[i]);
+class Libraries {
+  render() {
+    const display = document.getElementById('library');
+    display.innerHTML = ""; // To prevent duplicate copies
+    const books = document.querySelectorAll('book');
+    books.forEach(book => display.removeChild(book));
+    
+    let libraryBook = document.querySelector("#library");
+    // Go over the myLibrary array and display each book that's added
+    
+    for (let i = 0; i < myLibrary.length; i++) {
+      console.log(myLibrary[i]);
 
-    createBook(myLibrary[i]);
-  }
-}
-
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
-  let title = document.querySelector("#title").value;
-  let author = document.querySelector("#author").value;
-  let pages = document.querySelector("#pages").value;
-  let read = document.querySelector("#read").checked;
-  let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  render();
-
-  // Output the finished form
-  console.log(newBook);
-}
-
-// Make the New Button responsive
-let newBookbtn = document.querySelector("#new-book-btn");
-
-newBookbtn.addEventListener("click", function () {
-  let newBookForm = document.querySelector("#new-book-form");
-  newBookForm.style.display = "block";
-})
-
-// Show the output form
-document.querySelector("#new-book-form").addEventListener("submit", function (event) {
-  // Prevent page refreshing
-  event.preventDefault();
-  addBookToLibrary();
-})
-
-// Make book DOM elements to be used in render();
-function createBook(item) {
-  const library = document.querySelector('#library');
-  const bookDiv = document.createElement('div');
-  const titleDiv = document.createElement('div');
-  const authorDiv = document.createElement('div');
-  const pageDiv = document.createElement('div');
-  const readBtn = document.createElement('button');
-
-  bookDiv.classList.add('book');
-  bookDiv.setAttribute('id', myLibrary.indexOf(item));
-
-  titleDiv.textContent = item.title;
-  titleDiv.classList.add('title');
-  bookDiv.appendChild(titleDiv);
-
-  authorDiv.textContent = item.author;
-  authorDiv.classList.add('author');
-  bookDiv.appendChild(authorDiv);
-
-  pageDiv.textContent = item.pages;
-  pageDiv.classList.add('pages');
-  bookDiv.appendChild(pageDiv);
-
-  readBtn.classList.add('readBtn');
-  bookDiv.appendChild(readBtn);
-  if (item.read===false) {
-    readBtn.textContent = "Not read";
-    readBtn.style.backgroundColor = "red";
-  } else {
-    readBtn.textContent = "Read";
-    readBtn.style.backgroundColor = "green";
+      this.createBook(myLibrary[i]);
+    }
   }
 
-  library.appendChild(bookDiv);
+  addBookToLibrary() {
+    // take params, create a book then store it in the array
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").checked;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    this.render();
 
-  // Toggle ability to each book read
-  readBtn.addEventListener('click', () => {
-    item.read = !item.read;
-    setData();
-    render();
-  });
-};
-
-// Set Library to be stored in local storage
-function setData() {
-  localStorage.getItem('myLibrary', JSON.stringify(myLibrary));
-}
-
-
-// To load the saved data back by reading from localStorage and calls render
-function loadData() {
-  if (localStorage.getItem('mylibrary')) {
-    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+    // Output the finished form
+    console.log(newBook);
   }
-  render();
+
+  // Make book DOM elements to be used in render();
+  createBook(item) {
+    const library = document.querySelector('#library');
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const pageDiv = document.createElement('div');
+    const readBtn = document.createElement('button');
+
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('id', myLibrary.indexOf(item));
+
+    titleDiv.textContent = item.title;
+    titleDiv.classList.add('title');
+    bookDiv.appendChild(titleDiv);
+
+    authorDiv.textContent = item.author;
+    authorDiv.classList.add('author');
+    bookDiv.appendChild(authorDiv);
+
+    pageDiv.textContent = item.pages;
+    pageDiv.classList.add('pages');
+    bookDiv.appendChild(pageDiv);
+
+    readBtn.classList.add('readBtn');
+    bookDiv.appendChild(readBtn);
+    if (item.read===false) {
+      readBtn.textContent = "Not read";
+      readBtn.style.backgroundColor = "red";
+    } else {
+      readBtn.textContent = "Read";
+      readBtn.style.backgroundColor = "green";
+    }
+
+    library.appendChild(bookDiv);
+
+    // Toggle ability to each book read
+    readBtn.addEventListener('click', () => {
+      item.read = !item.read;
+      this.setData();
+      this.render();
+    });
+  }
+
+  // Set Library to be stored in local storage
+  setData() {
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  }
+
+
+  // To load the saved data back by reading from localStorage and calls render
+  loadData() {
+    if (localStorage.getItem('myLibrary')) {
+      myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+    }
+    this.render();
+  }
+
+  init() {
+    // Make the New Button responsive
+    let newBookbtn = document.querySelector("#new-book-btn");
+
+    newBookbtn.addEventListener("click", function () {
+      let newBookForm = document.querySelector("#new-book-form");
+      newBookForm.style.display = "block";
+    })
+    // Show the output form
+    document.querySelector("#new-book-form").addEventListener("submit", (event) => /* Arrow function*/ {
+      // Prevent page refreshing
+      event.preventDefault();
+      this.addBookToLibrary();
+    })
+
+  }
+
 }
-loadData(); // Call it when page loads
+
+const library = new Libraries();
+library.loadData(); // Call it when page loads
+library.init();
